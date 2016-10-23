8f2ccff00d0256e75cd43922cf7742b5d2d622c6
Michael Barbour <barbour.michael.0@gmail.com>
2016-10-23 17:43:44 -0400

    Add JSON output support and '-i' flag.
    Credit to mborn319 for the JSON output idea and initial implemenation
    of it.

--- musicbrainz_discid.py
+++ musicbrainz_discid.py
@@ -1,54 +1,109 @@
 #!/usr/bin/env python2
 
-# Requires python-discid and python-musicbrainzngs
-
 import os
 import sys
+import getopt
 import discid
 import musicbrainzngs as ws
 
-if len(sys.argv) > 2 or (len(sys.argv) == 2 and sys.argv[1] == "-h"):
-	print("usage: {0:s} [device]".format(os.path.basename(sys.argv[0])))
+use_json = False
+compact_json = True
+extra_includes = []
+
+def usage():
+	print("Usage: {0:s} [-hjJ] [-i list] [device]".format(os.path.basename(sys.argv[0])))
+	print("Flags:")
+	print("  -h       Show this help")
+	print("  -j       Use compact JSON output format")
+	print("  -J       Use pretty-printed JSON output format")
+	print("  -i list  Set extra includes (comma separated)")
+
+# note: calling json_print() more than once will result in invalid JSON output
+def json_print(obj):
+	import json
+	if compact_json:
+		print(json.dumps(obj, separators=(',', ':')))
+	else:
+		print(json.dumps(obj, indent=2))
+
+def die(str):
+	if use_json:
+		json_print({"success": False, "message": str})
+	else:
+		print("error: " + str)
 	exit(1)
-elif len(sys.argv) == 2:
-	device = sys.argv[1]
-else:
-	device = discid.get_default_device();
 
-print("Device: " + device)
+try:
+	optlist, args = getopt.gnu_getopt(sys.argv[1:], "hjJi:")
+except getopt.GetoptError as e:
+	print("error: " + str(e))
+	usage()
+	exit(2)
+for o, a in optlist:
+	if o == "-h":
+		usage()
+		exit(1)
+	elif o in ("-j", "-J"):
+		use_json = True
+		compact_json = (o == "-j")
+	elif o == "-i":
+		extra_includes = a.split(",")
+device = discid.get_default_device() if len(args) == 0 else args[len(args) - 1]
 
-disc = discid.read(device=device)
-print("Disc ID: " + disc.id)
-print("TOC:     " + disc.toc_string)
+try:
+	disc = discid.read(device=device)
+except discid.DiscError as e:
+	die(str(e))
+if not use_json:
+	print("Device:  " + device)
+	print("Disc ID: " + disc.id)
+	print("TOC:     " + disc.toc_string)
 
 ws.set_useragent("musicbrainz_discid", "0.1")
-r = ws.get_releases_by_discid(disc.id, toc=disc.toc_string, cdstubs=False, includes=["artists", "recordings", "labels"])
-
-if 'disc' in r and r['disc']['release-count'] > 0:
-	print("Disc ID found")
+includes = ["artists", "recordings", "labels"] + extra_includes
+try:
+	r = ws.get_releases_by_discid(disc.id, toc=disc.toc_string, cdstubs=False, includes=includes)
+except ws.MusicBrainzError as e:
+	die(str(e))
+if 'disc' in r and len(r['disc']['release-list']) > 0:
+	search_type = "discid"
 	releases = r['disc']['release-list']
 elif 'release-list' in r and len(r['release-list']) > 0:
-	print("TOC search succeeded")
+	search_type = "toc"
 	releases = r['release-list']
 else:
-	print("Release not found")
-	exit(1)
+	die("Release not found")
 
-rnum = 1
-for x in releases:
-	print("Release " + str(rnum) + ":")
-	print("  ID:     " + x['id'])
-	print("  URL:    http://musicbrainz.org/release/" + x['id'])
-	print("  Title:  " + x['title'] + " ["
-		+ (x['release-event-list'][0]['date'] if 'release-event-list' in x and 'date' in x['release-event-list'][0] else "<no date>")
-		+ ", " + (x['country'] if 'country' in x else "<no country>")
-		+ ", " + (x['label-info-list'][0]['label']['name'] if x['label-info-count'] > 0 else "<no label>") + "]")
-	print("  Artist: " + x['artist-credit'][0]['artist']['name'])
-	print("  Tracks:")
-	for x in x['medium-list']:
-		print("    " + (x['format'] if 'format' in x else "Medium") + " " + str(x['position']) + ":")
-		tnum = 1
-		for x in x['track-list']:
-			print("      " + "{0:02d} ".format(tnum) + x['recording']['title'])
-			tnum += 1
-	rnum += 1
+if use_json:
+	toc = list(map(int, disc.toc_string.split(None)))
+	response = {
+		"success": True, "device": device, "search-type": search_type,
+		"disc": {
+			"release-list": releases, "sectors": str(disc.sectors), "offset-count": len(toc) - 3,
+			"release-count": len(releases), "offset-list": toc[3:], "id": disc.id
+		}
+	}
+	json_print(response)
+else:
+	if search_type == "discid":
+		print("Disc ID found")
+	elif search_type == "toc":
+		print("Fuzzy TOC search succeeded")
+	rnum = 1
+	for x in releases:
+		print("Release " + str(rnum) + ":")
+		print("  ID:     " + x['id'])
+		print("  URL:    http://musicbrainz.org/release/" + x['id'])
+		print("  Title:  " + x['title'] + " ["
+			+ (x['release-event-list'][0]['date'] if 'release-event-list' in x and 'date' in x['release-event-list'][0] else "<no date>")
+			+ ", " + (x['country'] if 'country' in x else "<no country>")
+			+ ", " + (x['label-info-list'][0]['label']['name'] if x['label-info-count'] > 0 else "<no label>") + "]")
+		print("  Artist: " + x['artist-credit'][0]['artist']['name'])
+		print("  Tracks:")
+		for x in x['medium-list']:
+			print("    " + (x['format'] if 'format' in x else "Medium") + " " + str(x['position']) + ":")
+			tnum = 1
+			for x in x['track-list']:
+				print("      " + "{0:02d} ".format(tnum) + x['recording']['title'])
+				tnum += 1
+		rnum += 1
